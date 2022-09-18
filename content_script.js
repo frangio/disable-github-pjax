5b907df2f05f1af2b5b0b865f5e77bb0c798661a
Francisco Giordano <frangio.1@gmail.com>
2022-09-18 19:16:42 -0300

    disable on click

--- content_script.js
+++ content_script.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function (e) {
-  const meta = document.head.querySelector('meta[name="enabled-features"]');
-  meta.content += ',PJAX_ENABLED';
-});
+let disabled = false;
+
+function disablePjax(e) {
+  if (!disabled) {
+    const meta = document.head.querySelector('meta[name="enabled-features"]');
+    meta.content += ',PJAX_ENABLED';
+    disabled = true;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', disablePjax, { once: true });
+document.addEventListener('click', disablePjax, { once: true, capture: true });
